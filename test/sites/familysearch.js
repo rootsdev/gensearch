e0270e9161b3204c2bb0bbfc135e5fc1c1cca479
Markus Henn <brezelman+github@mailbox.org>
2019-07-29 20:39:38 +0200

    Using new FamilySearch API

--- test/sites/familysearch.js
+++ test/sites/familysearch.js
@@ -3,15 +3,15 @@ var test = require('../tester.js')('familysearch');
 describe('familysearch', function(){
   
   it('1', function(){
-    test(test.data[0], 'https://www.familysearch.org/search/record/results?count=20&query=%2Bgivenname%3A%22Joe%20William%22~%20%2Bsurname%3AClark~');
+    test(test.data[0], 'https://www.familysearch.org/search/record/results?count=20&givenname=Joe%20William&surname=Clark');
   });
   
   it('2', function(){
-    test(test.data[1], 'https://www.familysearch.org/search/record/results?count=20&query=%2Bgivenname%3A%22Joe%20William%22~%20%2Bsurname%3AClark~%20%2Bbirth_place%3ATexas~%20%2Bdeath_place%3A%22Springfield%2C%20Illinois%22~%20%2Bfather_givenname%3ADale~%20%2Bfather_surname%3AClark~%20%2Bmother_givenname%3ASusan~%20%2Bmother_surname%3AAnthony~%20%2Bspouse_givenname%3AJennifer~%20%2Bspouse_surname%3AThomas~%20%2Bmarriage_place%3A%22St%20Louis%2C%20MO%22~%20%2Bbirth_year%3A1833-1837~%20%2Bdeath_year%3A1887-1891~%20%2Bmarriage_year%3A1856-1860~');
+    test(test.data[1], 'https://www.familysearch.org/search/record/results?count=20&givenname=Joe%20William&surname=Clark&birth_place=Texas&death_place=Springfield%2C%20Illinois&father_givenname=Dale&father_surname=Clark&mother_givenname=Susan&mother_surname=Anthony&spouse_givenname=Jennifer&spouse_surname=Thomas&marriage_place=St%20Louis%2C%20MO&birth_year_from=1833&birth_year_to=1837&death_year_from=1887&death_year_to=1891&marriage_year_from=1856&marriage_year_to=1860');
   });
   
   it('config', function(){
-    test(test.data[1], 'https://www.familysearch.org/search/record/results?count=20&query=%2Bgivenname%3A%22Joe%20William%22~%20%2Bsurname%3AClark~%20%2Bbirth_place%3ATexas~%20%2Bdeath_place%3A%22Springfield%2C%20Illinois%22~%20%2Bfather_givenname%3ADale~%20%2Bfather_surname%3AClark~%20%2Bmother_givenname%3ASusan~%20%2Bmother_surname%3AAnthony~%20%2Bspouse_givenname%3AJennifer~%20%2Bspouse_surname%3AThomas~%20%2Bmarriage_place%3A%22St%20Louis%2C%20MO%22~%20%2Bbirth_year%3A1832-1838~%20%2Bdeath_year%3A1885-1893~%20%2Bmarriage_year%3A1853-1863~', {
+    test(test.data[1], 'https://www.familysearch.org/search/record/results?count=20&givenname=Joe%20William&surname=Clark&birth_place=Texas&death_place=Springfield%2C%20Illinois&father_givenname=Dale&father_surname=Clark&mother_givenname=Susan&mother_surname=Anthony&spouse_givenname=Jennifer&spouse_surname=Thomas&marriage_place=St%20Louis%2C%20MO&birth_year_from=1832&birth_year_to=1838&death_year_from=1885&death_year_to=1893&marriage_year_from=1853&marriage_year_to=1863', {
       birthRange: 3,
       deathRange: 4,
       marriageRange: 5
@@ -19,7 +19,7 @@ describe('familysearch', function(){
   });
   
   it('collection', function(){
-    test(test.data[0], 'https://www.familysearch.org/search/record/results?count=20&query=%2Bgivenname%3A%22Joe%20William%22~%20%2Bsurname%3AClark~&collection_id=1473014', {
+    test(test.data[0], 'https://www.familysearch.org/search/record/results?count=20&givenname=Joe%20William&surname=Clark&collection_id=1473014', {
       collectionId: 1473014
     });
   });
